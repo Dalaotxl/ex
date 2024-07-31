9e3a2f2c2ce9dfbac63ac02e423046b0dea7b92e
221220000-Zhang San <zhangsan@foo.com>
2024-07-31 18:49:23 +0800

    creat main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void sort(int *arr,int size)
+{
+
+}
+int main()
+{
+    int arr[] = {22,45,67,35,54,29};
+    int size = sizeof(arr[])/sizeof(arr[0]);
+    sort(arr,size);
+    return 0;
+}
